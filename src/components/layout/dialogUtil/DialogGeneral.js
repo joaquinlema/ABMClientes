8390503b8aae1704d66d81322b5f9f5439add36b
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-05-20 11:44:41 -0300

    agregamos componenete de dialogo general para reutilizar

---
+++ src/components/layout/dialogUtil/DialogGeneral.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import { DialogContent } from '@material-ui/core';
+import { DialogTitle } from '@material-ui/core';
+
+export default function DialogoGeneral({open,   onClose, tituloFormulario, formulario}) {
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{tituloFormulario}</DialogTitle>
+        <DialogContent>
+            {formulario}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
