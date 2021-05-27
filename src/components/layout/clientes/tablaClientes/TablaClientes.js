37dcaf691c856d2a98d296ab81c0ae67441fcac8
Joaquin Lema <joaquin.lema.sp@SofttekProjects.com>
2021-05-27 11:39:08 -0300

    subimos la tabla reutilizable

--- src/components/layout/clientes/tablaClientes/TablaClientes.js
+++ src/components/layout/clientes/tablaClientes/TablaClientes.js
@@ -1,12 +1,22 @@
 import { Grid } from '@material-ui/core';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import HeaderPage from '../../utils/header/HeaderPage';
 import NuevoCliente from '../tablaClientes/nuevo/NuevoCliente';
 import TablaUtil from '../../utils/tabla/TablaUtil';
 import {  IconButton } from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
+import { useDispatch, useSelector } from 'react-redux';
+import {getClients} from '../../../../actions/ClienteActions';
 
 const TablaClientes = () => {
+
+    const {clientes} = useSelector(state => state.ClienteReducer);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getClients());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     const columns = ["Id", "Name","Apellido","Email",
       {
@@ -28,18 +38,6 @@ const TablaClientes = () => {
       }
     ];
       
-    const rows = [
-    { Id: 1, Name: 'Snow', Apellido: 'Jon', Email: 35, edit: 1},
-    { Id: 2, Name: 'Lannister', Apellido: 'Cersei', Email: 42, edit: 12 },
-    { Id: 3, Name: 'Lannister', Apellido: 'Jaime', Email: 45 ,edit: 13},
-    { Id: 4, Name: 'Stark', Apellido: 'Arya', Email: 16 ,edit: 14},
-    { Id: 5, Name: 'Targaryen', Apellido: 'Daenerys', Email: null ,edit: 15},
-    { Id: 6, Name: 'Melisandre', Apellido: null, Email: 150,edit: 16 },
-    { Id: 7, Name: 'Clifford', Apellido: 'Ferrara', Email: 44 ,edit: 17},
-    { Id: 8, Name: 'Frances', Apellido: 'Rossini', Email: 36 ,edit: 18},
-    { Id: 9, Name: 'Roxie', Apellido: 'Harvey', Email: 65 ,edit: 19},
-    ];
-      
     return (
         <Fragment>
             <Grid container item xs={12} sm={12} md={12} lg={12}>
@@ -47,7 +45,7 @@ const TablaClientes = () => {
             </Grid>
             <Grid item xs={12} sm={12} md={12} lg={12}>
                 {/* <EnhancedTable /> */}
-                <TablaUtil rows={rows} columns={columns} title={'Clientes'}/>
+                <TablaUtil rows={clientes} columns={columns} title={'Clientes'}/>
             </Grid>
         </Fragment>
     );
